eec552bce1fe39201f05010a80edf65bb02d475f
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-06-16 20:58:39 -0300

    options

---
+++ Gerencianet.SDK/APIAuthPair.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gerencianet.SDK
+{
+    /// <summary>
+    /// ID and Secret for authentication calls on endpoints
+    /// </summary>
+    public struct APIAuthPair
+    {
+        public string id { get; set; }
+        public string secret { get; set; }
+    }
+}
---
+++ Gerencianet.SDK/APIOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gerencianet.SDK
+{
+    public class APIOptions
+    {
+        /// <summary>
+        /// Authentication Pair to Production Environment
+        /// </summary>
+        public APIAuthPair AuthProduction;
+
+        /// <summary>
+        /// Authentication Pair to Development Environment
+        /// </summary>
+        public APIAuthPair AuthDevelopment;
+
+        /// <summary>
+        /// Authentication Pair to Playground Environment
+        /// </summary>
+        public APIAuthPair AuthPlayground;
+    }
+}
