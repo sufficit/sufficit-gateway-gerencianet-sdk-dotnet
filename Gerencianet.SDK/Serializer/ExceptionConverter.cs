1dc2d31c925f06be8345e4a2d2b14fe8c237da8d
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-04-10 23:42:44 -0300

    exception serializer

--- Gerencianet.SDK/Serializer/ExceptionConverter.cs
+++ Gerencianet.SDK/Serializer/ExceptionConverter.cs
@@ -6,19 +6,42 @@ using System.Text.Json.Serialization;
 
 namespace GerencianetSDK.Serializer
 {
-    public class ExceptionConverter : JsonConverter<Exception>
+    public class ExceptionConverter : JsonConverter<Exception?>
     {
-        public override Exception Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override Exception? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions _)
         {
-            throw new NotImplementedException();
+            if (reader.TokenType == JsonTokenType.Null)
+                return null;
+
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                // reads parsed data as string
+                string value = reader.GetString()!;
+                return new Exception(value);
+            }
+            /*
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                string value = reader;
+                return new Exception(value);
+            }*/
+
+            throw new NotImplementedException($"Unsupported token type: {reader.TokenType}");
         }
 
-        public override void Write(Utf8JsonWriter writer, Exception value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, Exception? value, JsonSerializerOptions options)
         {
+            if (value == null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+
             writer.WriteStartObject();
-            writer.WriteString("Message", value.Message);
-            // Add any other propoerties that you may want to include in your JSON.
-            // ...
+
+            writer.WriteString("type", value.GetType().Name);
+            writer.WriteString("message", value.Message);
+
             writer.WriteEndObject();
         }
     }
